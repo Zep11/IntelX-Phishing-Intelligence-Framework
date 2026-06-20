e032c05a6160e0365902ac85eba9241f1e8079fb
Shubrajit Dey <shuvadey.slg@gmail.com>
2026-06-21 02:35:51 +0530

    Updated parsing algorithm

--- parser.py
+++ parser.py
@@ -1,15 +1,37 @@
 from urllib.parse import urlparse
 import tldextract   
+import re
+
+#Rule for IP check
+def ip_check(hostname):
+    ip_pattern= r"^\d{1,3}(\.\d{1,3}){3}$"
+    return bool(re.match(ip_pattern , hostname))
+
 
 def parse_url(url):
     parsed = urlparse(url)
-    extracted = tldextract.extract(url)
+    hostname = parsed.netloc                                       
+    if ip_check(hostname):                                #IP Check is conducted here
 
-    return{
-        "scheme" : parsed.scheme , 
-        "domain" :  f"{extracted.domain}.{extracted.suffix}" ,
-        "subdomain" : extracted.subdomain ,
-        "path" : parsed.path , 
-        "query" : parsed.query
+        return{
+            "is_ip" : True ,
+            "scheme" : parsed.scheme , 
+            "domain" : None , 
+            "hostname" : hostname,
+            "subdomain" : None ,
+            "path" : parsed.path , 
+            "query" : parsed.query
+        }
+    else:    
+        extracted = tldextract.extract(url)
+
+        return{
+            "is_ip" : False ,
+            "scheme" : parsed.scheme , 
+            "domain" :  f"{extracted.domain}.{extracted.suffix}" ,
+            "subdomain" : extracted.subdomain ,
+            "hostname" : None , 
+            "path" : parsed.path , 
+            "query" : parsed.query
     }
 
