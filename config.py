d84b6d8b9dfeeaebbc516f2ce1830e3c7d44ea8a
Shubrajit Dey <shuvadey.slg@gmail.com>
2026-07-09 03:11:28 +0530

    Updated the Config File

--- config.py
+++ config.py
@@ -15,12 +15,35 @@ def setup_config():                                              #if not > addin
         mxtoolbox_key = input("\nMXToolbox API Key (Press Enter to Skip)\n> ")
     else:
         print("Threat Intelligence disabled ")
+        vt_key = ""
+        urlscan_key = ""
+        abuse_key = ""
+        otx_key = ""
+        gsb_key = ""
+        mxtoolbox_key = ""
+        
+
+    with open(".env", "w") as file:
+        file.write(f"VIRUSTOTAL_API_KEY={vt_key}\n")
+        file.write(f"URLSCAN_API_Key={urlscan_key}\n ")
+        file.write(f"ABUSEIPDB_API_KEY={abuse_key}\n")
+        file.write(f"OTX_API_KEY={otx_key}\n")
+        file.write(f"GOOGLE_SAFE_BROWSING_API_KEY={gsb_key}\n")
+        file.write(f"MXTOOLBOX_API_KEY={mxtoolbox_key}\n")
+    print("\nConfiguration loaded successfully.\n")
+
+
 
 def load_config():
     load_dotenv()
-    vt_key = os.getenv("VIRUSTOTAL_API_KEY")
-    urlscan_key = os.getenv("URLSCAN_API_KEY")                 #not the Old variable , after the key is load > a new variable is used 
-    abuse_key = os.getenv("ABUSEIPDB_API_KEY")
-    otx_key = os.getenv("OTX_API_KEY")
-    gsb_key = os.getenv("GOOGLE_SAFE_BROWSING_API_KEY")
-    mxtoolbox_key = os.getenv("MXTOOLBOX_API_KEY")
+    return{ 
+    "vt_key" : os.getenv("VIRUSTOTAL_API_KEY"),
+    "urlscan_key" : os.getenv("URLSCAN_API_KEY"),                 #not the Old variable , after the key is load > a new variable is used 
+    "abuse_key" : os.getenv("ABUSEIPDB_API_KEY"),
+    "otx_key" : os.getenv("OTX_API_KEY"),
+    "gsb_key" : os.getenv("GOOGLE_SAFE_BROWSING_API_KEY"),
+    "mxtoolbox_key" : os.getenv("MXTOOLBOX_API_KEY"),   
+    }
+
+def api_check(api_keys):
+    return any(api_keys.values())
