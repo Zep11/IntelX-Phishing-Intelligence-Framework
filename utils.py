66f01e6b7a3d5f33f323785d6115124fedecd803
Shubrajit Dey <shuvadey.slg@gmail.com>
2026-07-10 02:56:17 +0530

    Updated reports sections

--- utils.py
+++ utils.py
@@ -6,12 +6,12 @@ def create_report_folders():
         "reports/raw_rprt/urlscan",
         "reports/raw_rprt/abuseipdb",
         "reports/raw_rprt/whoislookup",
-        "reports/raw_rprt/phishtank",
+        "reports/raw_rprt/google_safe_browsing",
         "reports/clean_rprt/virustotal",
         "reports/clean_rprt/urlscan",
         "reports/clean_rprt/abuseipdb",
         "reports/clean_rprt/whoislookup",
-        "reports/clean_rprt/phishtank"
+        "reports/clean_rprt/google_safe_browsing",
     ]
 
     for folder in folders:
